69dd98700cf9fe03aee91050bb8763a824eaa25b
Luiz-Correa <correa.luizbatista@gmail.com>
2020-12-24 11:36:57 -0300

    master

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '12346',
+                nome: 'Beltrana da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereadora'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '23',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeita', small: true}
+                ]
+            },
+        ]
+    }
+];
